daed907304ee24e744acbfdd606a727d85617c3f
unknown <zxskmj@gmail.com>
2023-04-06 14:11:58 +0900

    0406_update

--- graph/5251/5251.py
+++ graph/5251/5251.py
@@ -2,13 +2,23 @@
 
 
 import sys
-sys.stdin = open("sample_input.txt", "r")
+sys.stdin = open("./sample_input.txt", "r")
 
 
 T = int(input())
 for tc in range(1, T + 1):
     N, E = map(int, input().split())
+    graph = [[9999999]*(N+1) for _ in range(N+1)]
 
+    for _ in range(E):
+        s, e, w = map(int, input().split())
+        graph[s][e] = w
 
-    ans = 0
-    print(f'#{tc} {ans}')
+    dp = [0] + [9999999] * N
+
+    for start in range(N+1):
+        for end in range(N+1):
+            if dp[start] + graph[start][end] < dp[end]:
+                dp[end] = dp[start] + graph[start][end]
+
+    print(f'#{tc} {dp[N]}')
